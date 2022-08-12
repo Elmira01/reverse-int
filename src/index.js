7e42a6525105e49a27ab06c43d70a5769b00bb5f
Elmira Grigoryan <106161682+Elmira01@users.noreply.github.com>
2022-08-12 19:15:41 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+                 let isNegative = n < 0;  
+                let reverseAsString = Math.abs(n).toString().split('').reverse().join('');
+                let result = Number(reverseAsString);
+                return  result;
 }
