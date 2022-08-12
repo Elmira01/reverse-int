6f8139ef59cb668ceb42a9c04d3f7ae9d3d3c89d
Elmira Grigoryan <106161682+Elmira01@users.noreply.github.com>
2022-08-12 20:20:08 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let reverse = 0;
+        while(n > 0)
+        {
+          reverse= reverse* 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+        returnreverse;
+    }
 }
